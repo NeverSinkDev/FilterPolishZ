ad736ac03349bb2f9da7abb7cccd4e901adfdab8
Tobias H. // Tobnac <tobiashanitsch@yahoo.de>
2019-02-18 02:25:54 +0100

    started working in remaining item UI: copy/paste, display filtering and branchKey selection (the latter is basically working)

--- FilterPolishZ/ModuleWindows/ItemInfo/ItemInfoView.xaml.cs
+++ FilterPolishZ/ModuleWindows/ItemInfo/ItemInfoView.xaml.cs
@@ -31,19 +31,28 @@ namespace FilterPolishZ.ModuleWindows.ItemInfo
     {
         private int lastIndex = -1;
 
-        public EconomyRequestFacade EconomyData { get; set; } = EconomyRequestFacade.GetInstance();
-        public ItemInformationFacade ItemInfoData { get; set; } = ItemInformationFacade.GetInstance();
-        public ObservableCollection<KeyValuePair<string, ItemList<NinjaItem>>> UnhandledUniqueItems { get; private set; } = new ObservableCollection<KeyValuePair<string, ItemList<NinjaItem>>>();
+        public EconomyRequestFacade EconomyData { get; } = EconomyRequestFacade.GetInstance();
+        public ItemInformationFacade ItemInfoData { get; } = ItemInformationFacade.GetInstance();
+        public ObservableCollection<KeyValuePair<string, ItemList<NinjaItem>>> UnhandledUniqueItems { get; } = new ObservableCollection<KeyValuePair<string, ItemList<NinjaItem>>>();
+
+        private string currentBranchKey;
 
         public ItemInfoView()
         {
             InitializeComponent();
-            this.InitializeItemInformationData();
+            
+            var allBranchKeys = this.EconomyData.EconomyTierlistOverview.Keys;
+            this.currentBranchKey = allBranchKeys.First();
+            this.BranchKeyDisplaySelection.ItemsSource = allBranchKeys;
+            this.BranchKeyDisplaySelection.SelectedIndex = 0;
+            
             this.DataContext = this;
         }
 
         private void InitializeItemInformationData()
         {
+            this.UnhandledUniqueItems.Clear();
+            
             this.EconomyData.EconomyTierlistOverview[this.GetBranchKey()]
                 .Where(x => !this.ItemInfoData.EconomyTierListOverview[this.GetBranchKey()].ContainsKey(x.Key))
                 .ToList().ForEach(z => this.UnhandledUniqueItems.Add(z));
@@ -88,7 +97,7 @@ namespace FilterPolishZ.ModuleWindows.ItemInfo
             var index = ItemInfoGrid.SelectedIndex;
             if (index != this.lastIndex)
             {
-                (InnerView as ItemVariationListView).SelectFirstItem();
+                InnerView.SelectFirstItem();
             }
         }
 
@@ -140,6 +149,40 @@ namespace FilterPolishZ.ModuleWindows.ItemInfo
             this.ItemInfoData.MigrateAspectDataToEcoData(this.EconomyData, branchKey);
         }
         
-        private string GetBranchKey() => "uniques"; // todo
+        private string GetBranchKey() => this.currentBranchKey;
+
+        private void OnCopyButtonClick(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("s"); // todo: find currently selected item and copy their ???? + enable paste button
+        }
+
+        private void OnPasteButtonClick(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("s"); // todo: find currently selected item and paste the saved data into that item
+        }
+
+        private void OnDisplayFilterChange(object sender, SelectionChangedEventArgs e)
+        {
+//            Console.WriteLine("s"); // todo: update the displayed list to only show <<<see selected mode>>>
+//            
+//            ListBoxItem lbi = ((sender as System.Windows.Controls.ListBox).SelectedItem as ListBoxItem);
+//            var s = lbi.Content.ToString();
+//            // todo: none of this works!!
+        }
+
+        private void OnBranchKeyChange(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems[0] is ComboBoxItem selected) // ((sender as System.Windows.Controls.ComboBox).SelectedItem as ComboBoxItem)
+            {
+                var newValue = selected.Content as string; //name
+                this.currentBranchKey = newValue;
+                this.InitializeItemInformationData();
+            }
+            else if (e.AddedItems[0] is string branchKey)
+            {
+                this.currentBranchKey = branchKey;
+                this.InitializeItemInformationData();
+            }
+        }
     }
 }
