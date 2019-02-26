27a6c7c5aac7102d33e0b049ae641b849443c329
Tobias H. // Tobnac <tobiashanitsch@yahoo.de>
2019-02-26 21:44:54 +0100

    added nullCheck for the saved item data file

--- FilterPolishZ/ModuleWindows/ItemInfo/ItemInfoView.xaml.cs
+++ FilterPolishZ/ModuleWindows/ItemInfo/ItemInfoView.xaml.cs
@@ -37,7 +37,7 @@ namespace FilterPolishZ.ModuleWindows.ItemInfo
         public static string CurrentBranchKey { get; set; } // static because other windows need to access this without having this instance
         private string currentDisplayFiltering;
         private bool isOnlyDisplayingMultiBases;
-        private HashSet<string> visitedBranches = new HashSet<string>(); // first visit to any branch should load that saved setup
+        private HashSet<string> visitedBranches = new HashSet<string>(); // to track which branches have already had their saved data loaded
 
         public ItemInfoView()
         {
@@ -173,6 +173,7 @@ namespace FilterPolishZ.ModuleWindows.ItemInfo
             var branchKey = this.GetBranchKey();
             
             var filePath = ItemInformationFacade.GetItemInfoSaveFilePath(leagueType, branchKey, baseStoragePath);
+            if (!System.IO.File.Exists(filePath)) return;
             var fileText = System.IO.File.ReadAllText(filePath);
             
             this.ItemInfoData.Deserialize(branchKey, fileText);
