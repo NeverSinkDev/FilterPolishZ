a699567e3b4f0ef8afd4bfd78852c53971bd2725
Tobias H. // Tobnac <tobiashanitsch@yahoo.de>
2019-04-26 22:02:38 +0200

    fully fixed invalid file name (currency->fossil)

--- FilterEconomy/Facades/ItemInformationFacade.cs
+++ FilterEconomy/Facades/ItemInformationFacade.cs
@@ -182,7 +182,7 @@ namespace FilterEconomy.Facades
         
         private string GetItemInfoSaveFilePath(string branchKey)
         {
-            if (branchKey == "unique->maps") branchKey = "uniqueMaps"; // todo: temp(?) fix to prevent illegal file name.
+            if (branchKey.Contains("->")) branchKey = branchKey.Replace("->", "");
             var directoryPath = $"{this.BaseStoragePath}/{this.LeagueType}";
             var fileName = $"{branchKey}.txt";
             var fileFullPath = $"{directoryPath}/{fileName}";
