67f3418c11e64a68bd8970e5b30285eaf5547c6e
Tobias H. // Tobnac <tobiashanitsch@yahoo.de>
2019-06-12 17:44:51 +0200

    fixed baseType data request by changing client

--- FilterCore/Constants/BaseTypeDataProvider.cs
+++ FilterCore/Constants/BaseTypeDataProvider.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 
 namespace FilterCore.Constants
@@ -11,8 +13,22 @@ namespace FilterCore.Constants
 
         private static Dictionary<string, Dictionary<string, string>> LoadData()
         {
-            var client = new WebClient();
-            var fullString = client.DownloadString(DataFileUrl);
+            // this client somehow stopped working. on one hand it suddenly needed a user agent specified, on the other it
+            // crashed because of endless redirects.
+//            var client = new WebClient();
+//            client.Headers.Add("user-agent", "any");  
+//            var fullString = client.DownloadString(DataFileUrl);
+            
+            var client = (HttpWebRequest) WebRequest.Create(DataFileUrl);
+            client.UserAgent = "any";
+            client.CookieContainer = new CookieContainer();
+
+            var fullString = "";
+            var reader = new StreamReader(client.GetResponse().GetResponseStream()); 
+            while (!reader.EndOfStream)
+            {
+                fullString += (reader.ReadLine()) + "\n";
+            }
 
             string[] stats = null;
             var baseTypeData = new Dictionary<string, Dictionary<string, string>>();
@@ -20,6 +36,8 @@ namespace FilterCore.Constants
             var isFirstLine = true;
             foreach (var line in fullString.Split('\n'))
             {
+                if (string.IsNullOrEmpty(line)) continue;
+                
                 var words = line.Split(',');
 
                 if (isFirstLine)
