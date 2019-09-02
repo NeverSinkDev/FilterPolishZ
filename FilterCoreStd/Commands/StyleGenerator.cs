a5397e5600d59651446c7f5e478310457e644d9d
NeverSink <JevJaku@gmail.com>
2019-09-02 22:18:21 +0200

    Fixed more errors for migration

--- FilterCoreStd/Commands/StyleGenerator.cs
+++ FilterCoreStd/Commands/StyleGenerator.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Media.Animation;
 using FilterCore.Constants;
 using FilterCore.Line;
 using FilterDomain.LineStrategy;
--- FilterCoreStd/FilterComponents/Line/LineStrategy/ColorLineStrategy.cs
+++ FilterCoreStd/FilterComponents/Line/LineStrategy/ColorLineStrategy.cs
@@ -1,10 +1,5 @@
 ﻿using FilterCore.Line;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Media;
 
 namespace FilterDomain.LineStrategy
 {
