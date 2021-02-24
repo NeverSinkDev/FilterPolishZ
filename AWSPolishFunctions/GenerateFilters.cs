db22660d56338d18750e4258a7817a5a8c66edbe
NeverSink <JevJaku@gmail.com>
2021-02-24 18:08:18 +0100

    WIP AWS

---
+++ AWSPolishFunctions/GenerateFilters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace AWSPolishFunctions
+{
+    public class GenerateFilters
+    {
+        public async Task<APIGatewayProxyResponse> FilterGenerationFunction(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
+        {
+            return new APIGatewayProxyResponse { Body = $"Hello + { apigProxyEvent.QueryStringParameters.First().Value.ToString() }" };
+        }
+    }
+}
